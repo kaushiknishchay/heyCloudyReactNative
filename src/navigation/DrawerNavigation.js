d8d9c04dfbd4182c94f69e2846fd07d7e7f4994e
SHolmes <kaushiknishchay@gmail.com>
2018-04-24 22:35:26 +0530

    Add Icon to DrawerLabel

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -1,54 +1,86 @@
 /* eslint-disable react/forbid-prop-types */
 import { DrawerNavigator } from 'react-navigation';
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import PropTypes from 'prop-types';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 
 import BackupScreen from '../components/BackupSettingScreen';
 import GalleryScreen from '../components/GalleryScreen';
 import HomeScreen from '../components/HomeScreen';
+import { accentColor } from '../constants/colors';
 
 
-const DrawerLabel = ({ children, ...props }) => (
-  <Text
+const DrawerLabel = ({ children, icon, ...props }) => (
+  <View
     style={{
-      margin: 16,
+    margin: 16,
+    flexDirection: 'row',
+    padding: 8,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  }}
+  >
+    {icon ? <MaterialIcons
+      name={icon}
+      size={25}
+      color={accentColor}
+      style={{
+        marginRight: 10,
+      }}
+    /> : null}
+    <Text
+      style={{
+      flex: 3,
+      marginLeft: 20 + icon ? 0 : 34,
       fontSize: 17,
-      paddingTop: 8,
-      paddingBottom: 8,
+      padding: 5,
+      lineHeight: 20,
       fontFamily: 'Roboto',
       fontWeight: 'bold',
       color: '#111',
     }}
-    {...props}
-  >
-    {children}
-  </Text>);
+      {...props}
+    >
+      {children}
+    </Text>
+  </View>);
 
 DrawerLabel.propTypes = {
   children: PropTypes.any.isRequired,
+  // eslint-disable-next-line react/require-default-props
+  icon: PropTypes.any,
 };
 
 const DrawerNavigation = DrawerNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
-      drawerLabel: (<DrawerLabel>Home</DrawerLabel>),
+      drawerLabel: (
+        <DrawerLabel icon="home">
+        Home
+        </DrawerLabel>),
       headerTitle: 'Home',
     },
   },
   Gallery: {
     screen: GalleryScreen,
     navigationOptions: {
-      drawerLabel: (<DrawerLabel>Gallery</DrawerLabel>),
+      drawerLabel: (
+        <DrawerLabel>
+        Gallery
+        </DrawerLabel>),
       headerTitle: 'Gallery',
     },
   },
   BackupSetting: {
     screen: BackupScreen,
     navigationOptions: {
-      drawerLabel: (<DrawerLabel>Settings</DrawerLabel>),
+      drawerLabel: (
+        <DrawerLabel icon="settings">
+          Settings
+        </DrawerLabel>),
       headerTitle: 'Settings',
     },
   },
