330f623d8d37473fcc7ef7329cd323bcf3e72439
N Kaushik <kaushiknishchay@gmail.com>
2018-04-24 16:47:06 +0530

    Add react-native-background-upload dependency in project
    Change buildTools version for android to 25.0.2 and targetSdkVersion

--- android/app/src/main/java/com/splashscreen/MainApplication.java
+++ android/app/src/main/java/com/splashscreen/MainApplication.java
@@ -3,6 +3,7 @@ package com.splashscreen;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.vydia.RNUploader.UploaderReactPackage;
 import com.jamesisaac.rnbackgroundtask.BackgroundTaskPackage;
 import com.rnfs.RNFSPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new UploaderReactPackage(),
             new BackgroundTaskPackage(),
             new RNFSPackage(),
             new RNFetchBlobPackage(),
