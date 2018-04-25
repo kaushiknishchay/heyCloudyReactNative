b01e111768a34bdb581f8326c637651c7466b411
N Kaushik <kaushiknishchay@gmail.com>
2018-04-25 13:30:25 +0530

    Add Redux Store Config

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 
 
+import configureStore from './src/store/store';
 import MainNavigation from './src/navigation/MainNavigation';
 import { statusBarColor } from './src/constants/colors';
 
+const store = configureStore();
+
 const App = props => (
-  <React.Fragment>
-    <StatusBar
-      backgroundColor={statusBarColor}
-      barStyle="light-content"
-    />
-    <MainNavigation />
-  </React.Fragment>
+  <Provider store={store}>
+    <React.Fragment>
+      <StatusBar
+        backgroundColor={statusBarColor}
+        barStyle="light-content"
+      />
+      <MainNavigation />
+    </React.Fragment>
+  </Provider>
 );
 export default App;
