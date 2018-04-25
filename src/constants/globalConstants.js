f18e38b52a43dae7e1756b7606f5f2f10b038541
N Kaushik <kaushiknishchay@gmail.com>
2018-04-25 14:37:29 +0530

    Add server availability check and handle error cases to call the 'callback' passed

---
+++ src/constants/globalConstants.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const SERVER_URL = 'http://localhost:3000/upload_raw';
--- src/service/manualFileUpload.js
+++ src/service/manualFileUpload.js
@@ -1,35 +1,58 @@
 import startFileUpload from './fileUpload';
 import realm, { getAllPhotosRealm } from '../database/realm';
 import fileUtil from '../utils/file';
+import isServerAvailable from '../utils/network';
 
 export default function doFileUpload(callBack = () => {}) {
-  const allPhotosRealm = getAllPhotosRealm();
-  if (allPhotosRealm !== undefined && allPhotosRealm.length > 0) {
-    const allPhotos = allPhotosRealm[0];
-    try {
-      // Get the folders to with backup property as true;
-      const backupFolders = [...realm.objects('Folder')].map(obj => obj.data);
+  isServerAvailable()
+    .then(() => {
+      const allPhotosRealm = getAllPhotosRealm();
+      if (allPhotosRealm !== undefined && allPhotosRealm.length > 0) {
+        const allPhotos = allPhotosRealm[0];
+        try {
+        // Get the folders to with backup property as true;
+          const backupFolders = [...realm.objects('Folder')].map(obj => obj.data);
 
-      if (allPhotos) {
-        const backupPhotosList = allPhotos.getPhotosToBackup(backupFolders);
+          if (allPhotos) {
+            const backupPhotosList = allPhotos.getPhotosToBackup(backupFolders);
 
-        backupPhotosList.forEach((obj, currentIndex) => {
-          const { uri } = obj.image;
-          fileUtil.getFileStat(uri)
-            .then((stats) => {
-              startFileUpload({
-                path: `${stats.path}`,
-                headers: {
-                  'content-type': obj.type,
-                  filePath: stats.path,
-                  fileName: stats.filename,
-                },
-              }, currentIndex, backupPhotosList.length, callBack);
+            backupPhotosList.forEach((obj, currentIndex) => {
+              const { uri } = obj.image;
+              fileUtil.getFileStat(uri)
+                .then((stats) => {
+                  startFileUpload({
+                    path: `${stats.path}`,
+                    headers: {
+                      'content-type': obj.type,
+                      filePath: stats.path,
+                      fileName: stats.filename,
+                    },
+                  }, currentIndex, backupPhotosList.length, callBack);
+                });
             });
-        });
+          }
+        } catch (e) {
+          realm.write(() => {
+            realm.create('BackupInfo', {
+              timestamp: new Date().toLocaleString(),
+              successful: false,
+              errorMessage: 'Error while uploading files',
+              errorFilesCount: 1,
+            }, true);
+          });
+          callBack();
+        }
       }
-    } catch (e) {
-      // console.error('Failed to Upload Files', e);
-    }
-  }
+    })
+    .catch(() => {
+      realm.write(() => {
+        realm.create('BackupInfo', {
+          timestamp: new Date().toLocaleString(),
+          successful: false,
+          errorMessage: 'Server is not reachable',
+          errorFilesCount: 1,
+        }, true);
+      });
+      callBack();
+    });
 }
