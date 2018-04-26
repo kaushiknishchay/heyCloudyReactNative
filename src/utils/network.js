add8656daee9a805d4c3b2f09785cc3a0c32a9c9
N Kaushik <kaushiknishchay@gmail.com>
2018-04-26 16:36:00 +0530

    Increase server reachable timeout to 500ms

--- src/utils/network.js
+++ src/utils/network.js
@@ -2,10 +2,9 @@ import { SERVER_URL } from '../constants/globalConstants';
 
 export default function isServerAvailable(url = SERVER_URL) {
   const timeout = new Promise((resolve, reject) => {
-    setTimeout(reject, 300, 'Request timed out');
+    setTimeout(reject, 500, 'Request timed out');
   });
 
   const request = fetch(url);
-
   return Promise.race([timeout, request]);
 }
