5dc4e7677b1db38824153351c2f772648cfcacea
N Kaushik <kaushiknishchay@gmail.com>
2018-04-26 17:04:43 +0530

    Fix error files count shown on HomeScreen

--- src/service/manualFileUpload.js
+++ src/service/manualFileUpload.js
@@ -1,7 +1,8 @@
-import startFileUpload from './fileUpload';
 import realm, { getAllPhotosRealm } from '../database/realm';
 import fileUtil from '../utils/file';
 import isServerAvailable from '../utils/network';
+import startFileUpload from './fileUpload';
+
 
 export default function doFileUpload(callBack = () => {}) {
   isServerAvailable()
@@ -11,11 +12,11 @@ export default function doFileUpload(callBack = () => {}) {
         const allPhotos = allPhotosRealm[0];
         try {
           // Get the folders to with backup property as true;
-          const backupFolders = [...realm.objects('Folder')].map(obj => obj.data);
+          const backupFolders = [...realm.objects('Folder').filtered('backedUp == true')]
+            .reduce((acc, obj) => `${acc === '' ? '' : `${acc} OR `} group_name=="${obj.data.name}"`, '');
 
           if (allPhotos) {
-            const backupPhotosList = allPhotos.getPhotosToBackup(backupFolders);
-
+            const backupPhotosList = allPhotos.edges.filtered(backupFolders);
             backupPhotosList.forEach((obj, currentIndex) => {
               const { uri } = obj.image;
               fileUtil.getFileStat(uri)
@@ -44,24 +45,29 @@ export default function doFileUpload(callBack = () => {}) {
         }
       }
     })
-    .catch(() => {
+    .catch((e) => {
       const allPhotosRealm = getAllPhotosRealm();
       let fileCount = 1;
+
       if (allPhotosRealm !== undefined && allPhotosRealm.length > 0) {
-        const allPhotos = allPhotosRealm[0];
-        const backupFolders = [...realm.objects('Folder')].map(obj => obj.data);
+        const backupFolders = [...realm.objects('Folder').filtered('backedUp == true')]
+          .reduce((acc, obj) => `${acc === '' ? '' : `${acc} OR `} group_name=="${obj.data.name}"`, '');
+
+        const allPhotos = allPhotosRealm[0].edges.filtered(backupFolders);
         if (allPhotos) {
-          fileCount = allPhotos.getPhotosToBackup(backupFolders).length;
+          fileCount = allPhotos.length;
         }
       }
 
       realm.write(() => {
+        realm.delete(realm.objects('BackupInfo'));
+
         realm.create('BackupInfo', {
           timestamp: new Date().toLocaleString(),
           successful: false,
           errorMessage: 'Server is not reachable',
           errorFilesCount: fileCount,
-        }, true);
+        });
       });
       callBack();
     });
